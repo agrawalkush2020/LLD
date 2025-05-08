4d78c0f75f5284e26386e56ab0d221af1f9ef4b7
kushagra agrawal <agrawalkush2020@gmail.com>
2025-05-09 02:16:44 +0530

    oberver bhi done

--- BestGithubDesignPatterns/design-patterns/BehaviroalPatterns/observer/Observer.cxx
+++ BestGithubDesignPatterns/design-patterns/BehaviroalPatterns/observer/Observer.cxx
@@ -7,94 +7,101 @@
 
 #include <iostream>
 #include <vector>
+using namespace std;
 
 class Subject;
 
 class Observer {
-public:
-    Observer(const int id) : id_(id) {}
-    virtual ~Observer() {}
+    public:
+        Observer(int id) : id_(id) {}
+        virtual ~Observer() {}
 
-    virtual int getState() = 0;
-    virtual void update(Subject* subject) = 0;
-    virtual int getId() = 0;
-protected:
-    int id_;
+        virtual int getState() const = 0;
+        virtual void update(Subject* subject) = 0;
+        virtual int getId() const = 0;
+
+    protected:
+        int id_;
 };
 
 class ConcreteObserver : public Observer {
-public:
-    ConcreteObserver(const int state, const int id)
-    : object_state_(state), Observer(id) {}
-    ~ConcreteObserver() {}
+    public:
+        ConcreteObserver(int state, int id)
+            : object_state_(state), Observer(id) {}
 
-    int getState() {
-        return object_state_;
-    }
+        ~ConcreteObserver() override {}
 
-    int getId() {
-        return id_;
-    }
+        int getState() const override {
+            return object_state_;
+        }
 
-    void update(Subject* subject);
-private:
-    int object_state_;
+        int getId() const override {
+            return id_;
+        }
+
+        void update(Subject* subject) override;
+
+    private:
+        int object_state_;
 };
 
 class Subject {
-public:
-    virtual ~Subject() {}
+    public:
+        virtual ~Subject() {}
 
-    void attach(Observer* observer) {
-        observers_.push_back(observer);
-    }
+        void attach(Observer* observer) {
+            observers_.push_back(observer);
+        }
 
-    void detach(const int id) {
-        for (auto it = observers_.begin(); it != observers_.end(); ++it) {
-            if ((*it)->getId() == id) {
-                observers_.erase(it);
+        void detach(int id) {
+            for (auto it = observers_.begin(); it != observers_.end(); ++it) {
+                if ((*it)->getId() == id) {
+                    observers_.erase(it);
+                    break;  // prevent iterator invalidation
+                }
             }
         }
-    }
 
-    void notify() {
-        for (auto observer : observers_) {
-            observer->update(this);
+        void notify() {
+            for (auto observer : observers_) {
+                observer->update(this);
+            }
         }
-    }
 
-    virtual int getState() = 0;
-    virtual void setState(const int state) = 0;
-private:
-    std::vector<Observer*> observers_;
+        virtual int getState() const = 0;
+        virtual void setState(int state) = 0;
+
+    protected:
+        vector<Observer*> observers_;
 };
 
 class ConcreteSubject : public Subject {
-public:
-    ~ConcreteSubject() {}
+    public:
+        ~ConcreteSubject() override {}
 
-    int getState() {
-        return subject_state_;
-    }
+        int getState() const override {
+            return subject_state_;
+        }
 
-    void setState(const int state) {
-        subject_state_ = state;
-    }
-private:
-    int subject_state_;
+        void setState(int state) override {
+            subject_state_ = state;
+        }
+
+    private:
+        int subject_state_ = 0;
 };
 
-void ConcreteObserver::update(Subject *subject) {
+void ConcreteObserver::update(Subject* subject) {
     object_state_ = subject->getState();
-    std::cout << "Observer(id=" << id_ << ") update state to: " << object_state_ << std::endl;
+    cout << "Observer(id=" << id_ << ") update state to: " << object_state_ << endl;
 }
 
 int main() {
     ConcreteObserver observer1(1000, 1);
     ConcreteObserver observer2(2000, 2);
 
-    std::cout << "Observer1 state: " << observer1.getState() << std::endl;
-    std::cout << "Observer2 state: " << observer2.getState() << std::endl;
+    cout << "Observer1 state: " << observer1.getState() << endl;
+    cout << "Observer2 state: " << observer2.getState() << endl;
 
     Subject* subject = new ConcreteSubject();
     subject->attach(&observer1);
@@ -103,13 +110,127 @@ int main() {
     subject->setState(10);
     subject->notify();
 
-    std::cout << "Observer1 state: " << observer1.getState() << std::endl;
-    std::cout << "Observer2 state: " << observer2.getState() << std::endl;
+    cout << "Observer1 state: " << observer1.getState() << endl;
+    cout << "Observer2 state: " << observer2.getState() << endl;
 
-    subject->detach(0);
+    subject->detach(1); // FIXED: detach observer1 using valid id
     subject->setState(100);
     subject->notify();
 
-    std::cout << "Observer1 state: " << observer1.getState() << std::endl;
-    std::cout << "Observer2 state: " << observer2.getState() << std::endl;
+    cout << "Observer1 state: " << observer1.getState() << endl;
+    cout << "Observer2 state: " << observer2.getState() << endl;
+
+    delete subject; // prevent memory leak
+    return 0;
 }
+
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// class Subject;
+
+// class Observer {
+//     public:
+//         Observer(const int id) : id_(id) {}
+//         virtual ~Observer() {}
+
+//         virtual int getState() = 0;
+//         virtual void update(Subject* subject) = 0;
+//         virtual int getId() = 0;
+//     protected:
+//         int id_;
+// };
+
+// class ConcreteObserver : public Observer {
+//     public:
+//         ConcreteObserver(const int state, const int id)
+//         : object_state_(state), Observer(id) {}
+//         ~ConcreteObserver() {}
+
+//         int getState() {
+//             return object_state_;
+//         }
+
+//         int getId() {
+//             return id_;
+//         }
+
+//         void update(Subject* subject);
+//     private:
+//         int object_state_;
+// };
+
+// class Subject {
+//     private:
+//         vector<Observer*> observers_;
+//     public:
+//         virtual ~Subject() {}
+
+//         void attach(Observer* observer) {
+//             observers_.push_back(observer);
+//         }
+
+//         void detach(const int id) {
+//             for (auto it = observers_.begin(); it != observers_.end(); ++it) {
+//                 if ((*it)->getId() == id) {
+//                     observers_.erase(it);
+//                 }
+//             }
+//         }
+
+//         void notify() {
+//             for (auto observer : observers_) {
+//                 observer->update(this);
+//             }
+//         }
+
+//         virtual int getState() = 0;
+//         virtual void setState(const int state) = 0;
+// };
+
+// class ConcreteSubject : public Subject {
+// public:
+//     ~ConcreteSubject() {}
+
+//     int getState() {
+//         return subject_state_;
+//     }
+
+//     void setState(const int state) {
+//         subject_state_ = state;
+//     }
+// private:
+//     int subject_state_;
+// };
+
+// void ConcreteObserver::update(Subject *subject) {
+//     object_state_ = subject->getState();
+//     cout << "Observer(id=" << id_ << ") update state to: " << object_state_ << endl;
+// }
+
+// int main() {
+//     ConcreteObserver observer1(1000, 1);
+//     ConcreteObserver observer2(2000, 2);
+
+//     cout << "Observer1 state: " << observer1.getState() << endl;
+//     cout << "Observer2 state: " << observer2.getState() << endl;
+
+//     Subject* subject = new ConcreteSubject();
+//     subject->attach(&observer1);
+//     subject->attach(&observer2);
+
+//     subject->setState(10);
+//     subject->notify();
+
+//     cout << "Observer1 state: " << observer1.getState() << endl;
+//     cout << "Observer2 state: " << observer2.getState() << endl;
+
+//     subject->detach(1);
+//     subject->setState(100);
+//     subject->notify();
+
+//     cout << "Observer1 state: " << observer1.getState() << endl;
+//     cout << "Observer2 state: " << observer2.getState() << endl;
+// }
